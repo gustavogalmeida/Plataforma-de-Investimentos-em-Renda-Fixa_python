b42cf29311a64eb871e2b1c742bd566d81efa24d
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-03-17 16:56:39 -0300

    style: corrigido titulo e nome do item do formulário que é resgatado

--- app/views.py
+++ app/views.py
@@ -15,14 +15,13 @@ def modalidades():
                            titulo='Lista de modalidades - Plataforma de investimentos')
 @app.route('/nova')
 def nova_modalidade():
-    return render_template('modalidades_novo.html', titulo='Nova modalidade')
+    return render_template('modalidades_novo.html', titulo='Formulário para cadastro de modalidades')
 
 @app.route('/gravar_modalidade', methods=['POST',])
 def gravar_modalidade():
-    codigo = request.form['codigo']
     descricao = request.form['descricao']
     tipo = request.form['tipo_group']
-    resgate = request.form['resgate_group']
+    resgate = request.form['permite_resgate_automatico']
     prazo_minimo = request.form['prazo_minimo']
     prazo_maximo = request.form['prazo_maximo']
     modalidade = models.Modalidades(codigo, descricao, tipo, resgate, prazo_minimo, prazo_maximo)
