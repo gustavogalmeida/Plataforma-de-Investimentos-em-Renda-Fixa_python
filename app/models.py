3407969f242e62611f37b4f7e7c87e340c49de8c
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-03-26 22:21:54 -0300

    fix: removido codigo, já que ele ainda vai ser gerado sequencialmente na parte do banco

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 class Modalidades:
-    def __init__(self, codigo: int, descricao: str, tipo: str, resgate_automatico: str,
+    def __init__(self, descricao: str, tipo: str, resgate_automatico: str,
                  prazo_minimo: int, prazo_maximo: int):
-        self.codigo = codigo
         self.descricao = descricao
         self.tipo = tipo
         self.resgate_automatico = resgate_automatico 
