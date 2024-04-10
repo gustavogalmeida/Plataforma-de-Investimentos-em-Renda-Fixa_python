67a0b8b3b9d45223167afc9c2303e8109ee17a1d
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-09 23:56:14 -0300

    feat: implementado model com sql alchemy

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from __main__ import db
+
 class Modalidades:
     def __init__(self, descricao: str, tipo: str, resgate_automatico: str,
                  prazo_minimo: int, prazo_maximo: int):
@@ -6,3 +8,11 @@ class Modalidades:
         self.resgate_automatico = resgate_automatico 
         self.prazo_minimo = prazo_minimo
         self.prazo_maximo = prazo_maximo
+
+class Modalidades(db.Model): 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(30), nullable=False)
+    tipo = db.Column(db.String(30), nullable=False)
+    resgate_automatico = db.Column(db.String(15), nullable=False)
+    prazo_minimo = db.Column(db.Integer, nullable=False)
+    prazo_maximo = db.Column(db.Integer, nullable=False)
