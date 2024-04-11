9fd3f8524e0c16ad3b232ba55b66dde0c32ed5e5
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-11 20:49:21 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = '3155'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:admin@localhost/db_investimentos'
-
 db = SQLAlchemy(app)
+
+from app import views
