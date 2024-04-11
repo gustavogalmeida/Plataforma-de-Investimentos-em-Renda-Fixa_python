2ba3c4468a492874274a47fe063daa54b773fabe
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-10 21:05:17 -0300

    fix: adaptando codigo para implementação feita com ORM

--- app/models.py
+++ app/models.py
@@ -1,14 +1,5 @@
 from __main__ import db
 
-class Modalidades:
-    def __init__(self, descricao: str, tipo: str, resgate_automatico: str,
-                 prazo_minimo: int, prazo_maximo: int):
-        self.descricao = descricao
-        self.tipo = tipo
-        self.resgate_automatico = resgate_automatico 
-        self.prazo_minimo = prazo_minimo
-        self.prazo_maximo = prazo_maximo
-
 class Modalidades(db.Model): 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(30), nullable=False)
