dea20ad64c4a6c57c722df7dceba680d7731cfca
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-25 07:55:53 -0300

    chore: correção nome do banco e imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:admin@localhost/db_investimentos'
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:admin@localhost/db_plataforma_investimentos'
 db = SQLAlchemy(app)
 
 from app import views
