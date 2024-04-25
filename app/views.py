b78d24f45cae4c1a9f5bc17dcb8b07f4ac81382c
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-25 07:58:47 -0300

    feat: implementado rotas para calcular, cadastrar, excluir e editar simulações

--- app/views.py
+++ app/views.py
@@ -8,14 +8,69 @@ modalidade_lista = []
 def index():
     return render_template('index.html'), 200
 
+@app.route('/simulacao', methods=['POST', 'GET'])
+def simulacao():
+    simulacoes_lista = models.Simulacao.query.order_by(models.Simulacao.id)
+
+    if request.method=='POST':
+        valor_aplicado = request.form['valor']
+        dias = request.form['dias']
+        cdi_pago = request.form['cdi']
+
+        cdi_com_sobras = helpers.calcula_cdi_considerando_sobras(cdi_pago)
+        rentabilidade_valor = helpers.calcula_rentabilidade_periodo_valor(dias, valor_aplicado, cdi_pago)
+        valor_sobras = helpers.calcula_valor_sobras(valor_aplicado)
+        rentabilidade_bruta_valor = helpers.calcula_rentabilidade_bruta_valor(valor_sobras, rentabilidade_valor)
+        rentabilidade_bruta_porcentagem_aa = helpers.calcula_rentabilidade_bruta_porcentagem_aa(rentabilidade_bruta_valor, valor_aplicado)
+        saldo_total = helpers.calcula_saldo_total(rentabilidade_bruta_valor, valor_aplicado)
+        return render_template('simulacao.html', 
+                            simulacoes_lista=simulacoes_lista,
+                            valor_aplicado=valor_aplicado,
+                            dias=dias,
+                            rentabilidade_paga=rentabilidade_valor, 
+                            cdi=cdi_pago,
+                            cdi_considerando_sobras=cdi_com_sobras,
+                            valor_sobras=valor_sobras,
+                            rentabilidade_bruta_valor=rentabilidade_bruta_valor,
+                            saldo_total=saldo_total,
+                            rentabilidade_bruta_porcentagem_aa=rentabilidade_bruta_porcentagem_aa) 
+    return render_template('simulacao.html', simulacoes_lista=simulacoes_lista) 
+
+@app.route('/salvar_simulacao', methods=['POST', ])
+def salvar_simulacao():
+    descricao = request.form['descricao']
+    valor_aplicado = request.form['valor_aplicado']
+    cdi = request.form['cdi']
+    cdi_sobras = request.form['cdi_considerando_sobras']
+    dias = request.form['dias']
+    rentabilidade_bruta = request.form['rentabilidade_bruta_valor']
+    saldo_total = request.form['saldo_total']
+
+    nova_simulacao = models.Simulacao(descricao=descricao, valor_aplicado=valor_aplicado, cdi=cdi, cdi_sobras=cdi_sobras, dias=dias, rentabilidade_bruta=rentabilidade_bruta, saldo_total=saldo_total)
+
+    db.session.add(nova_simulacao)
+    db.session.commit()
+
+    return redirect(url_for('simulacao'))
+
+@app.route('/excluir_simulacao/<int:id>')
+def excluir_simulacao(id):
+    models.Simulacao.query.filter_by(id=id).delete()
+    db.session.commit()
+    return redirect(url_for('simulacao'))
+
+@app.route('/editar_simulacao')
+def editar():
+    ## TODO: implementar rota de edição (será necessário outra pag)
+    pass
+
 @app.route('/modalidades')
 def modalidades():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         flash('Você precisa estar conectado para acessar este recurso!')
         return redirect (url_for('login'))       
     return render_template('modalidades.html',
-                           modalidades_lista = modalidade_lista,
-                           titulo='Lista de modalidades - Plataforma de investimentos')
+                           modalidades_lista = modalidade_lista)
 
 @app.route('/gravar_modalidade', methods=['POST',])
 def gravar_modalidade():
@@ -37,7 +92,7 @@ def gravar_modalidade():
 
 @app.route('/login')
 def login():
-    return render_template('login.html', titulo='Faça seu login para continuar')
+    return render_template('login.html')
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
