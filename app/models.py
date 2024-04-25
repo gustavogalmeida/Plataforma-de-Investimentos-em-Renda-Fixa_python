1d6be3abf9bc114aca8217af9332fead61a562f3
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-04-25 07:56:36 -0300

    feat: novo modelo para o banco - Simulacao

--- app/models.py
+++ app/models.py
@@ -12,3 +12,16 @@ class Usuarios(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     nome = db.Column(db.String(70), nullable=False)
     senha = db.Column(db.String(30), nullable=False)
+
+class Simulacao(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    descricao = db.Column(db.String(30), nullable=False)
+    valor_aplicado = db.Column(db.Float, nullable=False)
+    cdi = db.Column(db.Float, nullable=False)
+    cdi_sobras = db.Column(db.Float, nullable=False)
+    dias = db.Column(db.Integer, nullable=False)
+    rentabilidade_bruta = db.Column(db.Integer, nullable=False)
+    saldo_total = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
